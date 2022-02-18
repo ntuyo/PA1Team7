8d116886327d0355b09329e7ba704068f589e8ca
Nazari T <nazarituyo@gmail.com>
2022-02-18 01:34:14 -0500

    Video Commit

--- pa01-team7/schedule.py
+++ pa01-team7/schedule.py
@@ -16,7 +16,7 @@ class Schedule():
     def load_courses(self):
         ''' load_courses reads the course data from the courses.json file'''
         print('getting archived regdata from file')
-        with open("courses20-21.json","r",encoding='utf-8') as jsonfile:
+        with open("/Users/nazarituyo/Desktop/Brandeis/GitHub/PA1Team7/pa01-team7/courses20-21.json","r",encoding='utf-8') as jsonfile:
             courses = json.load(jsonfile)
         for course in courses:
             course['instructor'] = tuple(course['instructor'])
